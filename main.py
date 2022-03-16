11a56b3d004bfc1bbac4e521a0bac38c0750c674
assasinfil <nfa57643@gmail.com>
2022-03-16 13:25:44 +0300

    refactoring

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ from os import walk
 from skimage.transform import resize
 import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 DS_DIR = "testsets/classic5"
 
 
-def readImages(directory=""):
+def read_images(directory=""):
     directory += "./"
     filenames = next(walk(directory), (None, None, []))[2]
     images = []
@@ -22,28 +20,25 @@ def readImages(directory=""):
     return images
 
 
-def createDataset(images):
+def create_dataset(images):
     # создаём датасет из картинок
-    #number = random.randint(0, len(images))
-    dataSet = np.empty(shape=(len(images), len(images[0]), len(images[0][0]), 1), dtype=np.float32)
-    print(dataSet.shape)
-    # dataSet = np.empty([2, 2], dtype=int)
+    # number = random.randint(0, len(images))
+    data_set = np.empty(shape=(len(images), len(images[0]), len(images[0][0]), 1), dtype=np.float32)
+    print(data_set.shape)
+    # data_set = np.empty([2, 2], dtype=int)
     for n in range(0, len(images)):
         for i in range(0, len(images[n])):
             for j in range(0, len(images[n][i])):
-                dataSet[n][i][j][0] = (images[n][i][j])
-        # dataSet.append([inputImgs[n]])
-    return dataSet
+                data_set[n][i][j][0] = (images[n][i][j])
+        # data_set.append([inputImgs[n]])
+    return data_set
 
 
 def readDS():
-    imgs = readImages(DS_DIR)
-    dataset = createDataset(imgs)
+    images = read_images(DS_DIR)
+    dataset = create_dataset(images)
     return dataset
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     readDS()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
