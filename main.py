2dd0ae76c453e2fe2f6859e924a08ba2421e30d7
Eszdman <Eszdman2015@gmail.com>
2022-03-22 17:54:11 +0300

    Added equalization

--- main.py
+++ main.py
@@ -26,6 +26,17 @@ def processImages(imgs):
         processed.append(img)
     return processed
 
+def equalize(image, number_bins=256):
+
+    # get image histogram
+    image_histogram, bins = np.histogram(image.flatten(), number_bins, density=True)
+    cdf = image_histogram.cumsum() # cumulative distribution function
+    cdf = 255 * cdf / cdf[-1] # normalize
+
+    # use linear interpolation of cdf to find new pixel values
+    image_equalized = np.interp(image.flatten(), bins[:-1], cdf)
+
+    return image_equalized.reshape(image.shape)
 def compareProcessed(imgsOriginal, imgsProcessed):
     PSNRs = np.empty(shape=(len(imgsOriginal)), dtype=np.float32)
     print("len:",len(imgsOriginal))
